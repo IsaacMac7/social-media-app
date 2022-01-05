95cdf31b6fa1470ea645bde43799d603c4bba209
IsaacMac <isaacmacandog7@gmail.com>
2022-01-05 11:06:30 +1100

    first commit
    - top bar
    - home page

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,42 @@
+import { Search, Person, Chat, Notifications } from "@material-ui/icons"
+import "./topbar.css" // "." means same file
+
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">SocialBook</span>
+            </div>
+            <div className="topbarCenter"></div>
+                <div className="searchbar">
+                    <Search/>
+                    <input placeholder="Search for friend, post or video" className="searchInput"/>
+                </div>
+            <div className="topbarRight">
+                <span className="topbarLink">Homepage</span>
+                <span className="topbarLink">Timeline</span>
+            </div>
+            <div className="topbarIcons">
+                <div className="topbarIconItem">
+                    <Person/>
+                    <span className="topbarIconBadge">1</span>
+                </div>
+                <div className="topbarIconItem">
+                    <Chat/>
+                    <span className="topbarIconBadge">2</span>
+                </div>
+                <div className="topbarIconItem">
+                    <Notifications/> 
+                    <span className="topbarIconBadge">3</span>
+                </div>
+            </div>
+            <img src="\assets\person\1.jpeg" alt="profile pic" className="topbarImg"/> 
+        </div>
+    )
+}
+
+/*
+To access img src, simply give image path. 
+By default, react will know its in public directory
+*/
---
+++ src/pages/home/Home.js
@@ -0,0 +1,7 @@
+import Topbar from "../../components/topbar/Topbar" //".." means to go out of current file
+
+export default function Home() {
+    return (
+        <Topbar/>
+    )
+}
