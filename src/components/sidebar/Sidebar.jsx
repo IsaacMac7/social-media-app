755d6ee45fdaeadc5cdb1d4fd458f56da122da71
IsaacMac <isaacmacandog7@gmail.com>
2022-01-05 17:08:53 +1100

    Sidebar fundamentals
    - Implemented side bar list and icons
    - needs fix on hover due to padding/margin structure
    - button is still static (no function)

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,60 @@
+/*
+Bugs to fix:
+- sidebar button is not complete
+
+*/
+
 import "./sidebar.css"
-import { RssFeed } from "@material-ui/icons" 
+import PeopleIcon from '@mui/icons-material/People';
+import GroupsIcon from '@mui/icons-material/Groups';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import HistoryIcon from '@mui/icons-material/History';
+
 
 export default function Sidebar() {
     return (
         <div className="sideBar">
             <div className="sidebarWrapper">
                 <ul className="sidebarList">
-                    <li className="sidebarListIem">
-                        <RssFeed className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Feed</span>
+                    <li className="sidebarListItem">
+                        <img src="/assets/person/1.jpeg" alt="profile pic" className="pfp"/>
+                        <span className="sidebarListItemText">Me</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PeopleIcon className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Friends</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <GroupsIcon className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <StorefrontIcon className="sidebarIcon"/>
+                        <span className="sidebarListItemText">MarketPlace</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <OndemandVideoIcon className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Watch</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <HistoryIcon className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Memories</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <button className="sidebarButton">^</button>
+                        <span className="sidebarListItemText">See More</span>
+                    </li>
+                </ul>
+                <hr className="sidebarHr" /> 
+                <ul className="sidebarShortcutList">
+                    <li className="sidebarShortcuts">
+                        <img src="/assets/appIcons/angrybirds.png" alt="angry birds app" className="shortcutIcons" />
+                        <span className="sidebarAppName">AngryBirds</span>
+                    </li>
+                    <li className="sidebarShortcuts">
+                        <img src="/assets/appIcons/amazon.png" alt="amazon app" className="shortcutIcons" />
+                        <span className="sidebarAppName">Amazon</span>
                     </li>
                 </ul>
             </div>
